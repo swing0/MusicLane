49da7f84121559997d41c634e609c65c7eb7657e
swing <45418055+swing0@users.noreply.github.com>
2022-08-30 17:38:12 +0800

    编辑模式改进

--- Scripts/EditPage/MoveCamera.cs
+++ Scripts/EditPage/MoveCamera.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class MoveCamera : MonoBehaviour
 {
     public bool canReSize = true;
+    public AudioSource audio;
     float scale = 0.5f;
+    float audioTime;
+    float beatTempo; // д╛хо180
     // Start is called before the first frame update
     void Start()
     {
-        
+        beatTempo = gameObject.GetComponentInParent<EditScroller>().beatTempo / 60f;
     }
 
     // Update is called once per frame
@@ -17,14 +20,19 @@ public class MoveCamera : MonoBehaviour
     {
         if (canReSize)
         {
-            if (Input.GetAxis("Mouse ScrollWheel") > 0)
+            if (Input.GetAxis("Mouse ScrollWheel") > 0 && transform.position.y <= audio.clip.length * beatTempo + 3)
             {
                 transform.position += new Vector3(0, 1 * scale, 0);
+                audioTime = audio.time + scale / beatTempo;
+                audio.time = audioTime;
             }
-            else if (Input.GetAxis("Mouse ScrollWheel") < 0)
+            else if (Input.GetAxis("Mouse ScrollWheel") < 0 && transform.position.y >= 3)
             {
                 transform.position -= new Vector3(0, 1 * scale, 0);
+                audioTime = audio.time - scale / beatTempo;
+                audio.time = audioTime;
             }
+
         }
     }
 }
